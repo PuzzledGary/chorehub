9e92a98e02c8128424472140346497bab464de9d
PuzzledGary <165698166+PuzzledGary@users.noreply.github.com>
2026-02-02 17:22:26 +0100

    Initial commit

---
+++ src/main/java/de/caransgar/chorehub/ChorehubApplication.java
@@ -0,0 +1,13 @@
+package de.caransgar.chorehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChorehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChorehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/caransgar/chorehub/ChorehubApplicationTests.java
@@ -0,0 +1,13 @@
+package de.caransgar.chorehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChorehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
